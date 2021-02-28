4654af2dd7e89c64c4033b9808ac618f235cd113
Gokul Alex <gokulsan@users.noreply.github.com>
2021-02-28 23:37:31 +0530

    Update SiddhiToken.sol

--- contracts/SiddhiToken.sol
+++ contracts/SiddhiToken.sol
@@ -1,325 +1,196 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache License 2.0
 
-pragma solidity >=0.6.2 <0.8.0;
+pragma solidity >=0.6.0 <0.8.0;
 pragma experimental ABIEncoderV2;
 
 import "./Context.sol";
-import "./IERC20.sol";
 import "./SafeMath.sol";
 import "./Address.sol";
 import "./ReentrancyGuard.sol";
 import "./mimc7.sol";
+import "./IERC165.sol";
+import "./IERC721.sol";
+import "./IERC721TokenReceiver.sol";
 
-contract SiddhiToken is Context, IERC20, ReentrancyGuard {
-    using SafeMath for uint256;
-    using Address for address;
+contract SiddhiToken is IERC721, ReentrancyGuard{
+    string public name = "Siddhi NFT Token";
+    string public symbol = "SIDDHI";
 
-    mapping (address => uint256) internal _balances;
-    mapping (address => bytes32) internal _balanceHashes;
+    uint256 private _totalTokens = 0;
+    mapping(uint256 => address) private tokenToOwner;
+    mapping(address => uint256) private tokensByOwner;
+    mapping(address => mapping(address => bool)) private authOperators;
+    mapping(uint256 => address) private approvedAddresses;
+    mapping(uint256 => Ticket) private tickets;
 
-    mapping (address => mapping (address => uint256)) private _allowances;
-
-    uint256 private _totalSupply;
-
-    string private _name;
-    string private _symbol;
-    uint8 private _decimals;
-
-
-    /**
-     * @dev Sets the values for {name} and {symbol}, initializes {decimals} with
-     * a default value of 18.
-     *
-     * To select a different value for {decimals}, use {_setupDecimals}.
-     *
-     * All three of these values are immutable: they can only be set once during
-     * construction.
-     */
-    constructor (string memory name, string memory symbol, uint8 decimals) public {
-        _name = name;
-        _symbol = symbol;
-        _decimals = decimals;
+    struct Ticket {
+        string name;
+        uint256 adn;
+        uint256 points;
+        bytes data;
     }
 
-    /**
-    @param in_x, array of inputs, inputs should be uint256
-    @param in_k, key used in the algorithm
-    @return uint256, returns the hash of the values contained in in_x */
-    function MiMCpe7_mp( uint256[] memory in_x, uint256 in_k )
-        internal pure returns (uint256)
-    {
-        uint256 r = in_k;
-        uint256 localQ = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+    constructor() {
+        _totalTokens++;
+        tokensByOwner[address(this)]++;
+        tickets[_totalTokens] = Ticket(
+            "GOLD TOKEN",
+            _totalTokens,
+            10000,
+            "KILLER"
+        );
+        tokenToOwner[_totalTokens] = address(this);
+    }
 
-        for ( uint256 i = 0; i < in_x.length; i++ )
-        {
-            r = (r + in_x[i] + MiMCpe7_generated.MiMCpe7(in_x[i], r)) % localQ;
+    function isContract(address addr) private view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(addr)
         }
-        
-        return r;
-    }
-    /**
-    @param value to hash
-    @return uint256, mimc hash of value */
-    function mimc(uint256 value) public view returns (uint256){
-        uint[] memory tmp = new uint[](1);
-        tmp[0] = value;
-        return MiMCpe7_mp(tmp, 2);
-    }
-    
-
-    /**
-     * @dev Returns the name of the token.
-     */
-    function name() public view returns (string memory) {
-        return _name;
+        return size > 0;
     }
 
-    /**
-     * @dev Returns the symbol of the token, usually a shorter version of the
-     * name.
-     */
-    function symbol() public view returns (string memory) {
-        return _symbol;
+    function balanceOf(address _owner)
+        external
+        view
+        override
+        returns (uint256)
+    {
+        require(_owner != address(0));
+        return tokensByOwner[_owner];
     }
 
-    /**
-     * @dev Returns the number of decimals used to get its user representation.
-     * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
-     *
-     * Tokens usually opt for a value of 18, imitating the relationship between
-     * Ether and Wei. This is the value {ERC20} uses, unless {_setupDecimals} is
-     * called.
-     *
-     * NOTE: This information is only used for _display_ purposes: it in
-     * no way affects any of the arithmetic of the contract, including
-     * {IERC20-balanceOf} and {IERC20-transfer}.
-     */
-    function decimals() public view returns (uint8) {
-        return _decimals;
+    function ownerOf(uint256 _tokenId)
+        external
+        view
+        override
+        returns (address)
+    {
+        address _tokenOwner = tokenToOwner[_tokenId];
+        require(_tokenOwner != address(0));
+        return _tokenOwner;
     }
 
-    /**
-     * @dev See {IERC20-totalSupply}.
-     */
-    function totalSupply() public view override returns (uint256) {
-        return _totalSupply;
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) private {
+        require(
+            msg.sender == tokenToOwner[_tokenId] ||
+                authOperators[tokenToOwner[_tokenId]][msg.sender] ||
+                msg.sender == approvedAddresses[_tokenId]
+        );
+        approvedAddresses[_tokenId] = address(0);
+
+        require(_from == tokenToOwner[_tokenId]);
+        require(_to != address(0));
+
+        // Require NFT to be a valid ID
+        require(_tokenId > 0 && _tokenId <= _totalTokens);
+
+        // Transafer (overflow issue to solve with safeMath)
+        tokensByOwner[_to]++;
+        tokensByOwner[_from]--;
+        tokenToOwner[_tokenId] = _to;
+
+        emit Transfer(_from, _to, _tokenId);
     }
 
-    /**
-     * @dev See {IERC20-balanceOf}.
-     */
-    function balanceOf(address account) public view override returns (uint256) {
-        
-        return _balances[account];
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId,
+        bytes memory data
+    ) external payable override {
+        _transfer(_from, _to, _tokenId);
+        if (isContract(_to)) {
+            require(
+                IERC721TokenReceiver(_to).onERC721Received(
+                    msg.sender,
+                    _from,
+                    _tokenId,
+                    data
+                ) == this.onERC721Received(msg.sender, _from, _tokenId, data)
+            );
+        }
     }
 
-    
-    /**
-     * @dev See {IERC20-transfer}.
-     *
-     * Requirements:
-     *
-     * - `recipient` cannot be the zero address.
-     * - the caller must have a balance of at least `amount`.
-     */
-    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-        _transfer(_msgSender(), recipient, amount);
-        return true;
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable override {
+        this.safeTransferFrom(_from, _to, _tokenId, "");
     }
 
-    /**
-     * @dev See {IERC20-allowance}.
-     */
-    function allowance(address owner, address spender) public view virtual override returns (uint256) {
-        return _allowances[owner][spender];
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable override {
+        _transfer(_from, _to, _tokenId);
     }
 
-    /**
-     * @dev See {IERC20-approve}.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function approve(address spender, uint256 amount) public virtual override returns (bool) {
-        _approve(_msgSender(), spender, amount);
-        return true;
+    function approve(address _approved, uint256 _tokenId)
+        external
+        payable
+        override
+    {
+        require(
+            msg.sender == tokenToOwner[_tokenId] ||
+                authOperators[tokenToOwner[_tokenId]][msg.sender]
+        );
+        approvedAddresses[_tokenId] = _approved;
+
+        emit Approval(msg.sender, _approved, _tokenId);
     }
 
-    /**
-     * @dev See {IERC20-transferFrom}.
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20};
-     *
-     * Requirements:
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for ``sender``'s tokens of at least
-     * `amount`.
-     */
-    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
-        _transfer(sender, recipient, amount);
-        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
-        return true;
+    function setApprovalForAll(address _operator, bool _approved)
+        external
+        override
+    {
+        authOperators[msg.sender][_operator] = _approved;
+
+        emit ApprovalForAll(msg.sender, _operator, _approved);
     }
 
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
-        return true;
+    function getApproved(uint256 _tokenId)
+        external
+        view
+        override
+        returns (address)
+    {
+        return approvedAddresses[_tokenId];
     }
 
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
-    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
-        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
-        return true;
+    function isApprovedForAll(address _owner, address _operator)
+        external
+        view
+        override
+        returns (bool)
+    {
+        return authOperators[_owner][_operator] || false;
     }
 
-    /**
-     * @dev Moves tokens `amount` from `sender` to `recipient`.
-     *
-     * This is internal function is equivalent to {transfer}, and can be used to
-     * e.g. implement automatic token fees, slashing mechanisms, etc.
-     *
-     * Emits a {Transfer} event.
-     *
-     * Requirements:
-     *
-     * - `sender` cannot be the zero address.
-     * - `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     */
-    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
-        require(sender != address(0), "ERC20: transfer from the zero address");
-        require(recipient != address(0), "ERC20: transfer to the zero address");
-
-        _beforeTokenTransfer(sender, recipient, amount);
-
-        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
-        _balances[recipient] = _balances[recipient].add(amount);
-        
-        _balanceHashes[sender] = bytes32(mimc( _balances[sender]));
-        _balanceHashes[recipient] = bytes32(mimc(_balances[recipient]));
-
-        emit Transfer(sender, recipient, amount);
+    function onERC721Received(
+        address _operator,
+        address _from,
+        uint256 _tokenId,
+        bytes memory _data
+    ) external pure returns (bytes4) {
+        return
+            bytes4(
+                keccak256(
+                    abi.encodePacked(
+                        "onERC721Received(",
+                        _operator,
+                        _from,
+                        _tokenId,
+                        _data,
+                        ")"
+                    )
+                )
+            );
     }
-
-    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-     * the total supply.
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * Requirements
-     *
-     * - `to` cannot be the zero address.
-     */
-    function _mint(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: mint to the zero address");
-
-        _beforeTokenTransfer(address(0), account, amount);
-
-        _totalSupply = _totalSupply.add(amount);
-        _balances[account] = _balances[account].add(amount);
-        
-        _balanceHashes[account] = bytes32(mimc(_balances[account]));
-        emit Transfer(address(0), account, amount);
-    }
-
-    /**
-     * @dev Destroys `amount` tokens from `account`, reducing the
-     * total supply.
-     *
-     * Emits a {Transfer} event with `to` set to the zero address.
-     *
-     * Requirements
-     *
-     * - `account` cannot be the zero address.
-     * - `account` must have at least `amount` tokens.
-     */
-    function _burn(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: burn from the zero address");
-
-        _beforeTokenTransfer(account, address(0), amount);
-
-        _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
-        
-        _balanceHashes[account] = bytes32(mimc(_balances[account]));
-        _totalSupply = _totalSupply.sub(amount);
-        emit Transfer(account, address(0), amount);
-    }
-
-    /**
-     * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
-     *
-     * This is internal function is equivalent to `approve`, and can be used to
-     * e.g. set automatic allowances for certain subsystems, etc.
-     *
-     * Emits an {Approval} event.
-     *
-     * Requirements:
-     *
-     * - `owner` cannot be the zero address.
-     * - `spender` cannot be the zero address.
-     */
-    function _approve(address owner, address spender, uint256 amount) internal virtual {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
-
-        _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
-    }
-
-    /**
-     * @dev Sets {decimals} to a value other than the default one of 18.
-     *
-     * WARNING: This function should only be called from the constructor. Most
-     * applications that interact with token contracts will not expect
-     * {decimals} to ever change, and may work incorrectly if it does.
-     */
-    function _setupDecimals(uint8 decimals_) internal {
-        _decimals = decimals_;
-    }
-
-    /**
-     * @dev Hook that is called before any transfer of tokens. This includes
-     * minting and burning.
-     *
-     * Calling conditions:
-     *
-     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-     * will be to transferred to `to`.
-     * - when `from` is zero, `amount` tokens will be minted for `to`.
-     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
 }
