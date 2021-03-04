251b215a530e02ad37a3ff60dd6300f05f6429cd
Gokul Alex <gokulsan@users.noreply.github.com>
2021-03-05 04:05:43 +0530

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
 //const mnemonic = fs.readFileSync(".secret").toString().trim();
-const mnemonic = 'lift salon admit gospel junk stomach cotton series own trend option steak';
+const mnemonic = '';
 
 module.exports = {
   networks: {
